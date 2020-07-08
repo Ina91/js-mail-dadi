06c0aeff65c6c28fcdeb0cd4daa400692f90f752
Ina91 <ina.met91@gmail.com>
2020-07-08 15:27:50 +0200

    sostituzione prompt alert

--- js/script.js
+++ js/script.js
@@ -4,14 +4,19 @@
 
 
 var mails = ['pinco@email.com' , 'pallo@email.com' , 'tizio@email.com' ,'caio@email.com' , 'sempronio@email.com', 'sole@email.com' , 'luna@email.com' , 'mare@email.com' , 'onda@email.com'];
+console.log(mails);
 var utente = prompt('Inserisci la tua email');
 var registro;
 
-for (var i = 0; i < mails.length; i++) {
-    if (utente === mails[i]){
-        registro = prompt('email accettata');
-
-    }else {
-        registro = alert('email non registrata');
+for (var i = 0; i < mails.length; i++)
+    {
+        if (utente === mails[i])
+            {
+            registro = alert('email accettata');
+            }
+            else
+                {
+                    registro = alert('email non registrata');
+                }
     }
-}
+// document.getElementById('utente').innerHTML = utente;
