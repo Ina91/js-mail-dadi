62a368ed371f41b3a7631f72bb18660238416ed3
Ina91 <ina.met91@gmail.com>
2020-07-09 17:15:51 +0200

    .

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -2,16 +2,15 @@ alert('Lancia il dado se il tuo numero è maggiore di quello della macchina vinc
 alert('Effettuare il lancio ? ');
 var x = Math.floor(Math.random()*6)+1; //numero casuale da 1 a 6 dado utente
 var y = Math.floor(Math.random()*6)+1;//numero casuale da 1 a 6 dado COMPUTER
+var risultato;
 document.getElementById('utente').innerHTML = x;
 document.getElementById('pc').innerHTML = y;
 
 if (x > y) {
-    var risultato = 'Hai vinto !';
-
-}else if (x == y) {
-    var risultato ='Questo è un pareggio !';
-}else {
-    var risultato = 'Hai perso !';
-}
-
+     risultato = 'Hai vinto !';
+}   else if (x == y) {
+        risultato ='Questo è un pareggio !';
+    }   else {
+            risultato = 'Hai perso !';
+        }
 document.getElementById('risultato').innerHTML =  risultato;
