d44c6c5eda3b755bd99bc3f1fc4628ebfe8801fd
Ina91 <ina.met91@gmail.com>
2020-07-09 14:44:22 +0200

    spostamento lancio del dato solo se l'email viene accetata.

--- mail/js/script.js
+++ mail/js/script.js
@@ -15,28 +15,29 @@ for (var i = 0; i < mails.length; i++)
     }
 if (controllo == true){
     alert('email accettata');
+
+    // Lancio Dado
+
+    alert('Lancia il dado se il tuo numero è maggiore di quello della macchina vinci.')
+    alert('Effetua il lancio');
+    var x = Math.floor(Math.random()*6)+1; //numero casuale da 1 a 6 dado utente
+    var y = Math.floor(Math.random()*6)+1;//numero casuale da 1 a 6 dado COMPUTER
+    document.getElementById('utente').innerHTML = x;
+    document.getElementById('pc').innerHTML = y;
+    if (x>y) {
+
+        var risultato = 'Hai vinto !';
+
+    }else if (x==y) {
+        var risultato ='Questo è un pareggio !';
+
+    }else {
+        var risultato = 'Hai perso !';
+    }
+
+    document.getElementById('risultato').innerHTML =  risultato;
+
     }
     else {
         alert('email non accettata');
     }
-
-// Lancio Dado
-
-alert('Lancia il dado se il tuo numero è maggiore di quello della macchina vinci.')
-alert('Effetua il lancio');
-var x = Math.floor(Math.random()*6)+1; //numero casuale da 1 a 6 dado utente
-var y = Math.floor(Math.random()*6)+1;//numero casuale da 1 a 6 dado COMPUTER
-document.getElementById('utente').innerHTML = x;
-document.getElementById('pc').innerHTML = y;
-if (x>y) {
-
-    var risultato = 'Hai vinto !';
-
-}else if (x==y) {
-    var risultato ='Questo è un pareggio !';
-
-}else {
-    var risultato = 'Hai perso !';
-}
-
-document.getElementById('risultato').innerHTML =  risultato;
