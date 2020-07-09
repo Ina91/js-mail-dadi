44ff8691d804ee1d2bc2a8d8b6ddcd2f11eb393f
Ina91 <ina.met91@gmail.com>
2020-07-09 17:00:17 +0200

    modifica estetica dado

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,13 +1,14 @@
-var lancio = alert('Lancia il dado')
+alert('Lancia il dado se il tuo numero è maggiore di quello della macchina vinci.');
+alert('Effettuare il lancio ? ');
 var x = Math.floor(Math.random()*6)+1; //numero casuale da 1 a 6 dado utente
 var y = Math.floor(Math.random()*6)+1;//numero casuale da 1 a 6 dado COMPUTER
 document.getElementById('utente').innerHTML = x;
 document.getElementById('pc').innerHTML = y;
 
-if (x>y) {
+if (x > y) {
     var risultato = 'Hai vinto !';
 
-}else if (x==y) {
+}else if (x == y) {
     var risultato ='Questo è un pareggio !';
 }else {
     var risultato = 'Hai perso !';
